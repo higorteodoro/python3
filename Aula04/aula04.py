27a68ccfcc6b38c1bf3102439c5ce18cf3472782
Higor Roberto <63817547+higorteodoro@users.noreply.github.com>
2022-04-10 22:46:51 -0300

    04 e 5

---
+++ Aula04/aula04.py
@@ -0,0 +1,23 @@
+"""
+Tipos de dados:
+
+Str - String - texto 
+Int - inteiro - 123 
+Float - real/ponto flutuante - com . 10.1
+bool - booleano/logico - true or false
+
+"""
+# String: Nome
+print('Higor Roberto Teodoro Rezende', type('Higor Roberto Teodoro Rezende'))
+
+# Idade: int
+i = 20
+print(i, type(i))
+
+# Altura: float
+print(1.76, type(1.76))
+
+# é maior de idade?
+print(i >= 18, type(i >= 18))
+
+
---
+++ aula05/aula05.py
@@ -0,0 +1,10 @@
+
+"""
++ adicaoo, - diminuicao, * multi, / divi, // divisao int, ** potenciacao,
+ % modulo resto divisao, () altrar a precedencia
+
+* serve tambem como repeticao
+"""
+print('Repeticao', 5 * 'Higor')
+
+print('Concatenacao', '5' + '5')
