874d14a5b05b5f3df5e3d92a80eadf9a1a4e565f
Higor Roberto <63817547+higorteodoro@users.noreply.github.com>
2022-05-10 08:01:58 -0300

    sim

--- desafios/desafio_02.py
+++ desafios/desafio_02.py
@@ -16,7 +16,6 @@ menos escreva "Seu nome e curo"; se tiver entre 5 e 6, escreva "Seu nome e norma
 Maior que 6 escreva "Seu nome e muito grande"
 
 """
-<<<<<<< HEAD
 """
 nome = input('Qual o seu nome: ')
 num = input('Digite um numero inteiro: ')
@@ -27,7 +26,7 @@ if resultado == 0:
 else:
     print(f'{nome} o numero digitado é {num} esse numero é impar')
 """    
- """       
+"""       
 nome = input('Qual o seu nome: ')
 quantidade = len(nome)
 
@@ -38,7 +37,6 @@ elif 5 >= quantidade <= 6:
 elif quantidade > 6:    
     print(f'Seu nome é normal')
 """ 
-=======
 
 nome = input('Digite seu nome: ')
 hora = float(input(f'Que horas sao {nome}?:'))
@@ -46,11 +44,9 @@ hora = float(input(f'Que horas sao {nome}?:'))
 
 if hora <= 11.59:
     print(f'Bom dia {nome}')
-elif 12 >= hora > 17.59:
+elif hora >= 12 and hora > 17.59:
     print(f'Boa tarde {nome}')
-elif 18 >= hora < 23.59:
+elif hora >= 18 and hora <= 23.59:
     print(f'Boa noite {nome}')
 
-
->>>>>>> c7ae09a098fca64d04a9c44fc3e10c5f8a97b8e9
 
